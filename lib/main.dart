62d76a36f41a59630cf807750162d6d6f07d0c65
fadliannaufal <fadliannaufal30@gmail.com>
2022-09-10 21:23:57 +0700

    video 7

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Row dan colummn"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Dimas"),
+            Text("Anjay"),
+            Text("Mabar Profesional"),
+            Row(
+              children: <Widget>[
+                Text("Dimas"),
+                Text("Anjay"),
+                Text("Mabar Profesional")
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
